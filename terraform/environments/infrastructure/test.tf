e3dfd2599f441f2269af6fb6a7e2c4443bd9c2a6
mdogui-deloitte <mdogui@deloitte.de>
2024-03-07 15:57:39 +0100

    changes

--- terraform/environments/infrastructure/test.tf
+++ terraform/environments/infrastructure/test.tf
@@ -5,5 +5,5 @@ resource "aws_s3_bucket" "my_bucket" {
   #checkov:skip=CKV_AWS_52:Skip reason
   #checkov:skip=CKV_AWS_21:Skip reason
   #checkov:skip=CKV_AWS_18:Skip reason
-  bucket = "my-test-new-bucket"
+  bucket = "my-test-new-bucket-1"
 }
