9656a3daac3389b24898fcd8560bea14b608fa0b
mdogui-deloitte <mdogui@deloitte.de>
2024-03-07 09:27:16 +0100

    update

--- terraform/environments/infrastructure/main.tf
+++ terraform/environments/infrastructure/main.tf
@@ -1,244 +1,151 @@
-########################################################################################################################
-###  Terraform and Providers setup
-########################################################################################################################
-terraform {
-  required_version = "= 0.14.8"
-  backend "s3" {
-    bucket = "data-eng-terraform-backend"
-    key    = "statefile.tfstate"
-    region = "eu-west-1"
-  }
-
-  required_providers {
-    aws = {
-      version = "= 4.19"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "3.1.1"
-    }
-
-  }
-}
-
-provider "null" {
-  # Configuration options
-}
-
-provider "aws" {
-  region = var.aws_region
-  default_tags {
-    tags = {
-      OrgScope        = "Not Set"
-      FunctionalScope = "Not Set"
-      Environment     = "Not Set"
-      ModuleName      = "Not Set"
-      GitRepository   = "github.com/Mohamed-Amine-Dogui/data-eng-project"
-      ProjectID       = var.project
-    }
-  }
-}
-
-########################################################################################################################
-###  data Human Roles
-########################################################################################################################
-data "aws_iam_role" "PA_DEVELOPER" {
-  name = "PA_DEVELOPER"
-}
-
-data "aws_iam_role" "PA_LIMITEDDEV" {
-  name = "PA_LIMITEDDEV"
-}
-
-data "aws_iam_role" "PA_CAS_demo_DATA_SCIENTIST" {
-  name = "PA_CAS_demo_DATA_SCIENTIST"
-}
-
-data "aws_iam_role" "PA_CAP_demo_DATA_SCIENTIST" {
-  name = "PA_CAP_demo_DATA_SCIENTIST"
-}
-
-data "aws_iam_role" "PA_CAP_demo_DEVELOPER" {
-  name = "PA_CAP_demo_DEVELOPER"
-}
-
-########################################################################################################################
-###  Convenient locals
-########################################################################################################################
-locals {
-  in_default_workspace  = terraform.workspace == "default"
-  in_production         = var.stage == "prd"
-  in_development        = var.stage == "dev"
-  in_integration        = var.stage == "int"
-  count_in_production   = local.in_production ? 1 : 0
-  count_in_default      = local.in_default_workspace ? 1 : 0
-  workspace_arn_prefix  = terraform.workspace != "default" && var.stage == "dev" ? "*" : ""
-  project_stage_pattern = "${local.workspace_arn_prefix}${var.project}-${var.stage}*"
-  account_id            = data.aws_caller_identity.current.account_id
-  access_arns           = compact(concat(!local.in_production ? [data.aws_iam_role.PA_DEVELOPER.arn, data.aws_iam_role.PA_CAP_demo_DEVELOPER.arn] : [], [data.aws_caller_identity.current.arn]))
-}
-
 #########################################################################################################################
-####  Remote state retrieval cap-consumer-ap
+####  Terraform and Providers setup
 #########################################################################################################################
-#data "terraform_remote_state" "cap_default" {
-#  backend   = "s3"
-#  workspace = "default"
-#  config = {
-#    // var.stage must be in ["prd", "int", "dev"]
-#    bucket = "cap-${var.stage}-terraform-backend"
-#    key    = "analytics-vpc-${var.aws_region}/cap/cap.tfstate"
-#    region = var.aws_region
+#terraform {
+#  required_version = "= 0.14.8"
+#  backend "s3" {
+#    bucket = "data-eng-terraform-backend"
+#    key    = "statefile.tfstate"
+#    region = "eu-west-1"
 #  }
-#  defaults = {}
+#
+#  required_providers {
+#    aws = {
+#      version = "= 4.19"
+#    }
+#    null = {
+#      source  = "hashicorp/null"
+#      version = "3.1.1"
+#    }
+#
+#  }
 #}
 #
-########################################################################################################################
-###  Remote state retrieval vwd
-########################################################################################################################
-#data "terraform_remote_state" "vwd_default_infrastructure" {
-#  backend = "s3"
-#  //  comment out if working on base resources that need to be in the feature
-#  //  workspace/branch.
-#  workspace = "default"
-#  config = {
-#    bucket = "vwd-dp-${var.stage}-terraform-backend"
-#    key    = "analytics-vpc-${var.aws_region}/infrastructure/infrastructure.tfstate"
-#    region = var.aws_region
+#provider "null" {
+#  # Configuration options
+#}
+#
+#provider "aws" {
+#  region = var.aws_region
+#  default_tags {
+#    tags = {
+#      OrgScope        = "Not Set"
+#      FunctionalScope = "Not Set"
+#      Environment     = "Not Set"
+#      ModuleName      = "Not Set"
+#      GitRepository   = "github.com/Mohamed-Amine-Dogui/data-eng-project"
+#      ProjectID       = var.project
+#    }
 #  }
 #}
-
-########################################################################################################################
-###  Convenience data retrieval
-########################################################################################################################
-data "aws_caller_identity" "current" {}
-
-########################################################################################################################
-###  generic resources Labels
-########################################################################################################################
-module "generic_labels" {
-  source         = "git::ssh://git@github.com/Mohamed-Amine-Dogui/tf-module-label.git?ref=tags/0.0.1"
-  git_repository = var.git_repository
-  project        = var.project
-  stage          = var.stage
-  layer          = var.stage
-  project_id     = var.project_id
-  kst            = var.tag_KST
-  wa_number      = var.wa_number
-
-  additional_tags = {
-    ApplicationID = "demo_onetom_generic"
-    ProjectID     = "demo"
-  }
-
-  resources = [
-    "monitoring_topic",
-    "notebook_user_pw"
-  ]
-}
-
-########################################################################################################################
-###  fsag Labels
-########################################################################################################################
-module "fsag_labels" {
-  source = "git::ssh://git@github.com/Mohamed-Amine-Dogui/tf-module-label.git?ref=tags/0.0.1"
-
-  project        = var.project
-  stage          = var.stage
-  name           = var.fsag_label
-  layer          = var.stage
-  project_id     = var.project_id
-  kst            = var.tag_KST
-  wa_number      = var.wa_number
-  git_repository = var.git_repository
-
-  additional_tags = {
-    ApplicationID = "demo_onetom_fast_track"
-    ProjectID     = "demo"
-  }
-  resources = [
-    "policy",
-    "glue-job",
-    "takeout-glue-job",
-    "redshift-conn",
-    "dlq-sns-topic",
-    "takeout-redshift-connection",
-    "secret",
-    "grant",
-    "kms"
-  ]
-}
-
-########################################################################################################################
-###  cc Labels
-########################################################################################################################
-module "cc_labels" {
-  source = "git::ssh://git@github.com/Mohamed-Amine-Dogui/tf-module-label.git?ref=tags/0.0.1"
-
-  project        = var.project
-  stage          = var.stage
-  name           = var.cc_label
-  layer          = var.stage
-  project_id     = var.project_id
-  kst            = var.tag_KST
-  wa_number      = var.wa_number
-  git_repository = var.git_repository
-
-  additional_tags = {
-    ApplicationID = "demo_onetom_campaign_cockpit"
-    ProjectID     = "demo"
-  }
-
-  resources = [
-    "policy",
-    "glue-job",
-    "takeout-glue-job",
-    "redshift-conn",
-    "dlq-sns-topic",
-    "takeout-redshift-connection"
-  ]
-}
-
-########################################################################################################################
-###  fstream Labels
-########################################################################################################################
-module "fstream_labels" {
-  source = "git::ssh://git@github.com/Mohamed-Amine-Dogui/tf-module-label.git?ref=tags/0.0.1"
-
-  project        = var.project
-  stage          = var.stage
-  name           = var.fstream_label
-  layer          = var.stage
-  project_id     = var.project_id
-  kst            = var.tag_KST
-  wa_number      = var.wa_number
-  git_repository = var.git_repository
-  additional_tags = {
-    ApplicationID = "demo_onetom_prod_tracker"
-    ProjectID     = "demo"
-  }
-
-  resources = [
-    "policy",
-    "glue-job",
-    "redshift-conn",
-    "takeout-redshift-connection",
-    "sqs",
-    "grant",
-    "kms",
-    "lambda"
-  ]
-}
-
-########################################################################################################################
-###  fasttrack Monitoring Labels
-########################################################################################################################
-#module "fasttrack_monitoring_labels" {
+#
+#########################################################################################################################
+####  data Human Roles
+#########################################################################################################################
+#data "aws_iam_role" "PA_DEVELOPER" {
+#  name = "PA_DEVELOPER"
+#}
+#
+#data "aws_iam_role" "PA_LIMITEDDEV" {
+#  name = "PA_LIMITEDDEV"
+#}
+#
+#data "aws_iam_role" "PA_CAS_demo_DATA_SCIENTIST" {
+#  name = "PA_CAS_demo_DATA_SCIENTIST"
+#}
+#
+#data "aws_iam_role" "PA_CAP_demo_DATA_SCIENTIST" {
+#  name = "PA_CAP_demo_DATA_SCIENTIST"
+#}
+#
+#data "aws_iam_role" "PA_CAP_demo_DEVELOPER" {
+#  name = "PA_CAP_demo_DEVELOPER"
+#}
+#
+#########################################################################################################################
+####  Convenient locals
+#########################################################################################################################
+#locals {
+#  in_default_workspace  = terraform.workspace == "default"
+#  in_production         = var.stage == "prd"
+#  in_development        = var.stage == "dev"
+#  in_integration        = var.stage == "int"
+#  count_in_production   = local.in_production ? 1 : 0
+#  count_in_default      = local.in_default_workspace ? 1 : 0
+#  workspace_arn_prefix  = terraform.workspace != "default" && var.stage == "dev" ? "*" : ""
+#  project_stage_pattern = "${local.workspace_arn_prefix}${var.project}-${var.stage}*"
+#  account_id            = data.aws_caller_identity.current.account_id
+#  access_arns           = compact(concat(!local.in_production ? [data.aws_iam_role.PA_DEVELOPER.arn, data.aws_iam_role.PA_CAP_demo_DEVELOPER.arn] : [], [data.aws_caller_identity.current.arn]))
+#}
+#
+##########################################################################################################################
+#####  Remote state retrieval cap-consumer-ap
+##########################################################################################################################
+##data "terraform_remote_state" "cap_default" {
+##  backend   = "s3"
+##  workspace = "default"
+##  config = {
+##    // var.stage must be in ["prd", "int", "dev"]
+##    bucket = "cap-${var.stage}-terraform-backend"
+##    key    = "analytics-vpc-${var.aws_region}/cap/cap.tfstate"
+##    region = var.aws_region
+##  }
+##  defaults = {}
+##}
+##
+#########################################################################################################################
+####  Remote state retrieval vwd
+#########################################################################################################################
+##data "terraform_remote_state" "vwd_default_infrastructure" {
+##  backend = "s3"
+##  //  comment out if working on base resources that need to be in the feature
+##  //  workspace/branch.
+##  workspace = "default"
+##  config = {
+##    bucket = "vwd-dp-${var.stage}-terraform-backend"
+##    key    = "analytics-vpc-${var.aws_region}/infrastructure/infrastructure.tfstate"
+##    region = var.aws_region
+##  }
+##}
+#
+#########################################################################################################################
+####  Convenience data retrieval
+#########################################################################################################################
+#data "aws_caller_identity" "current" {}
+#
+#########################################################################################################################
+####  generic resources Labels
+#########################################################################################################################
+#module "generic_labels" {
+#  source         = "git::ssh://git@github.com/Mohamed-Amine-Dogui/tf-module-label.git?ref=tags/0.0.1"
+#  git_repository = var.git_repository
+#  project        = var.project
+#  stage          = var.stage
+#  layer          = var.stage
+#  project_id     = var.project_id
+#  kst            = var.tag_KST
+#  wa_number      = var.wa_number
+#
+#  additional_tags = {
+#    ApplicationID = "demo_onetom_generic"
+#    ProjectID     = "demo"
+#  }
+#
+#  resources = [
+#    "monitoring_topic",
+#    "notebook_user_pw"
+#  ]
+#}
+#
+#########################################################################################################################
+####  fsag Labels
+#########################################################################################################################
+#module "fsag_labels" {
 #  source = "git::ssh://git@github.com/Mohamed-Amine-Dogui/tf-module-label.git?ref=tags/0.0.1"
 #
 #  project        = var.project
 #  stage          = var.stage
-#  name           = var.fasttrack_monitoring_label
+#  name           = var.fsag_label
 #  layer          = var.stage
 #  project_id     = var.project_id
 #  kst            = var.tag_KST
@@ -249,30 +156,123 @@ module "fstream_labels" {
 #    ApplicationID = "demo_onetom_fast_track"
 #    ProjectID     = "demo"
 #  }
+#  resources = [
+#    "policy",
+#    "glue-job",
+#    "takeout-glue-job",
+#    "redshift-conn",
+#    "dlq-sns-topic",
+#    "takeout-redshift-connection",
+#    "secret",
+#    "grant",
+#    "kms"
+#  ]
+#}
+#
+#########################################################################################################################
+####  cc Labels
+#########################################################################################################################
+#module "cc_labels" {
+#  source = "git::ssh://git@github.com/Mohamed-Amine-Dogui/tf-module-label.git?ref=tags/0.0.1"
+#
+#  project        = var.project
+#  stage          = var.stage
+#  name           = var.cc_label
+#  layer          = var.stage
+#  project_id     = var.project_id
+#  kst            = var.tag_KST
+#  wa_number      = var.wa_number
+#  git_repository = var.git_repository
+#
+#  additional_tags = {
+#    ApplicationID = "demo_onetom_campaign_cockpit"
+#    ProjectID     = "demo"
+#  }
 #
 #  resources = [
-#    "fast_track_lambda_alarm_event_rule",
-#    "fast_track_step_func_event_rule"
+#    "policy",
+#    "glue-job",
+#    "takeout-glue-job",
+#    "redshift-conn",
+#    "dlq-sns-topic",
+#    "takeout-redshift-connection"
 #  ]
-#
-#}
-
-########################################################################################################################
-###  Redshift
-########################################################################################################################
-#data "aws_security_group" "cap_redshift" {
-#  count = local.count_in_default
-#  id    = data.terraform_remote_state.cap_default.outputs.cap_redshift_cluster_base_security_group_id
 #}
 #
-#data "aws_redshift_cluster" "cap" {
-#  cluster_identifier = data.terraform_remote_state.cap_default.outputs.cap_redshift_cluster_identifier
+#########################################################################################################################
+####  fstream Labels
+#########################################################################################################################
+#module "fstream_labels" {
+#  source = "git::ssh://git@github.com/Mohamed-Amine-Dogui/tf-module-label.git?ref=tags/0.0.1"
+#
+#  project        = var.project
+#  stage          = var.stage
+#  name           = var.fstream_label
+#  layer          = var.stage
+#  project_id     = var.project_id
+#  kst            = var.tag_KST
+#  wa_number      = var.wa_number
+#  git_repository = var.git_repository
+#  additional_tags = {
+#    ApplicationID = "demo_onetom_prod_tracker"
+#    ProjectID     = "demo"
+#  }
+#
+#  resources = [
+#    "policy",
+#    "glue-job",
+#    "redshift-conn",
+#    "takeout-redshift-connection",
+#    "sqs",
+#    "grant",
+#    "kms",
+#    "lambda"
+#  ]
 #}
 #
-#data "aws_subnet" "cap_redshift_single_subnet" {
-#  id = split(",", data.terraform_remote_state.cap_default.outputs.cap_redshift_cluster_subnet_ids)[1]
-#}
+#########################################################################################################################
+####  fasttrack Monitoring Labels
+#########################################################################################################################
+##module "fasttrack_monitoring_labels" {
+##  source = "git::ssh://git@github.com/Mohamed-Amine-Dogui/tf-module-label.git?ref=tags/0.0.1"
+##
+##  project        = var.project
+##  stage          = var.stage
+##  name           = var.fasttrack_monitoring_label
+##  layer          = var.stage
+##  project_id     = var.project_id
+##  kst            = var.tag_KST
+##  wa_number      = var.wa_number
+##  git_repository = var.git_repository
+##
+##  additional_tags = {
+##    ApplicationID = "demo_onetom_fast_track"
+##    ProjectID     = "demo"
+##  }
+##
+##  resources = [
+##    "fast_track_lambda_alarm_event_rule",
+##    "fast_track_step_func_event_rule"
+##  ]
+##
+##}
 #
-#data "aws_ssm_parameter" "redshift_lambda_user_pw" {
-#  name = data.terraform_remote_state.cap_default.outputs.redshift_lambda_db_password_ssm_name
-#}
+#########################################################################################################################
+####  Redshift
+#########################################################################################################################
+##data "aws_security_group" "cap_redshift" {
+##  count = local.count_in_default
+##  id    = data.terraform_remote_state.cap_default.outputs.cap_redshift_cluster_base_security_group_id
+##}
+##
+##data "aws_redshift_cluster" "cap" {
+##  cluster_identifier = data.terraform_remote_state.cap_default.outputs.cap_redshift_cluster_identifier
+##}
+##
+##data "aws_subnet" "cap_redshift_single_subnet" {
+##  id = split(",", data.terraform_remote_state.cap_default.outputs.cap_redshift_cluster_subnet_ids)[1]
+##}
+##
+##data "aws_ssm_parameter" "redshift_lambda_user_pw" {
+##  name = data.terraform_remote_state.cap_default.outputs.redshift_lambda_db_password_ssm_name
+##}
